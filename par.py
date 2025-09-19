1a5f926874663e4af497184ef966659bb4803241
Diego Serra <diego.serra@gmail.com>
2025-09-18 21:28:33 -0300

    Primera version del progama para identificar numero par

---
+++ par.py
@@ -0,0 +1,9 @@
+def main():
+    num = int(input('Ingrese numero positivo para conocer su paridad: '))
+
+    if (num%2 == 0):
+        print('El numero es par.')
+    else:
+        print('El numero es impar.')
+
+main()
