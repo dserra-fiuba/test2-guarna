a0068abe9c8e123004d5b2fe74f2a5232164f70d
Diego Serra <diego.serra@gmail.com>
2025-09-18 21:47:01 -0300

    funcion para validar paridad

--- par.py
+++ par.py
@@ -1,10 +1,12 @@
 def solicitar_numero():
     return int(input('Ingrese numero positivo para conocer su paridad: '))
 
+def es_par(num):
+    return num%2 == 0
+
 def main():
     num = solicitar_numero()
-
-    if (num%2 == 0):
+    if (es_par(num)):
         print('El numero es par.')
     else:
         print('El numero es impar.')
